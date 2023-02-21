0e5d7734f1bfa2cc1b4a7738eb2349954f2a4e8e
Pooja R <pooja.ramesh@imaginelearning.com>
2023-02-21 13:16:16 +0530

    Adding Server side Functionalities

--- server-side/index.js
+++ server-side/index.js
@@ -1,7 +1,17 @@
 import express from "express";
 import Connection from "./database/db.js";
+import cors from "cors";
+import Routes from "./routes/route.js";
+import bodyParser from "body-parser";
 
 const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json({ extented: true }));
+app.use(bodyParser.urlencoded({ extented: true }));
+
+app.use("/", Routes);
 
 const PORT = 8000;
 
